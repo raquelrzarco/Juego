9e102a6122082a13005f10d3e8f6398ea91c07a7
raquelrzarco <raquel.zarco.97@gmail.com>
2017-03-28 10:40:10 +0200

    Menu lateral funciona, pero  no nos hace caso el escuchador de el icono, ya funciona los clicks y tenemos los framents montados. Hay que meterse en el java.

--- app/src/main/java/com/raquel/juego/MainActivity.java
+++ app/src/main/java/com/raquel/juego/MainActivity.java
@@ -3,6 +3,7 @@ package com.raquel.juego;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Toolbar toolbar;
     private DrawerLayout drawer;
     private NavigationView navView;
+    private FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +27,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer = (DrawerLayout) findViewById(R.id.drawer);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         navView = (NavigationView) findViewById(R.id.navView);
+        setSupportActionBar(toolbar);
 
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         navView.setNavigationItemSelectedListener(this);
+        
+        fm = getSupportFragmentManager();
+        fm.popBackStack(null,FragmentManager.POP_BACK_STACK_INCLUSIVE);
     }
 
     @Override
