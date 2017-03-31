3cd14c12aa2b87a297d46f34c97fd45afdbdd47e
raquelrzarco <raquel.zarco.97@gmail.com>
2017-03-31 14:14:05 +0200

    Perfil acabado

--- app/src/main/java/com/raquel/juego/fragments/PerfilFragment.java
+++ app/src/main/java/com/raquel/juego/fragments/PerfilFragment.java
@@ -1,20 +1,28 @@
 package com.raquel.juego.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.raquel.juego.Preferncias;
 import com.raquel.juego.R;
 import com.raquel.juego.activity.FormularioRegistroActivity;
+import com.raquel.juego.activity.PersonajesActivity;
+import com.raquel.juego.bean.PersonajeBean;
 import com.raquel.juego.bean.UsuarioBean;
 
 
-public class PerfilFragment extends Fragment {
+public class PerfilFragment extends Fragment{
+    private TextView txtperfilnombre;
+    private TextView txtperfilcorreo;
 
     public static PerfilFragment newInstance(){
         return new PerfilFragment();
@@ -33,6 +41,16 @@ public class PerfilFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_perfil, container, false);
         Preferncias preferencias = new Preferncias(getActivity());
         UsuarioBean usuarioBean = preferencias.getUsuario();
+
+        txtperfilnombre = (TextView) view.findViewById(R.id.txtperfilnombre);
+        txtperfilcorreo = (TextView) view.findViewById(R.id.txtperfilcorreo);
+
+        UsuarioBean usuariobean = new UsuarioBean();
+
+        txtperfilnombre.setText("Nombre de ususario: " + usuarioBean.getNombre());
+        txtperfilcorreo.setText("Correo de ususario: " + usuarioBean.getCorreo());
+
         return view;
     }
+
 }
